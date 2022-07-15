2bb540b7198e5935d4df0f3c105b7b1dca501865
SinghShivendra25 <Shivendra2.Singh@lntinfotech.com>
2022-07-15 15:51:13 +0530

    committing a dll file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MyMaths;
 
 namespace JenkinApp
 {
@@ -6,15 +7,10 @@ namespace JenkinApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Welcome to Jenkins");
-            Console.WriteLine("Hii my name is shivendra");
-            Console.WriteLine("I  am working on a jenkins");
-            Console.WriteLine("Enter two numbers");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"The sum of two given numbers is :{a + b}");
-            Console.ReadLine();
+            MyMath mm = new MyMath();
+            int a=mm.Add(2, 3);
+            int b = mm.Multiply(3, 5);
+            Console.WriteLine($"{a}..........{b}");
         }
     }
 }
