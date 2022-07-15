846c484580cd56ebd318ed7935174300b4d4de43
SinghShivendra25 <Shivendra2.Singh@lntinfotech.com>
2022-07-15 14:38:01 +0530

    Committed at 2:37 pm

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace JenkinApp
             Console.WriteLine("Welcome to Jenkins");
             Console.WriteLine("Hii my name is shivendra");
             Console.WriteLine("I  am working on a jenkins");
+            Console.WriteLine("Enter two numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The sum of two given numbers is :{a + b}");
             Console.ReadLine();
         }
     }
