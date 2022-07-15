8d145f47ce44507723699caf7ad2a31bcbb88ed1
SinghShivendra25 <Shivendra2.Singh@lntinfotech.com>
2022-07-15 14:29:50 +0530

    committed successfully

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace JenkinApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Jenkins");
+            Console.WriteLine("Hii my name is shivendra");
+            Console.WriteLine("I  am working on a jenkins");
             Console.ReadLine();
         }
     }
